b0e0279ed1b0f588f88fc4d2c47299ef09d22b97
SnehansuBehera <snehansbehera080903@gmail.com>
2024-08-10 02:10:33 +0530

    ui-update

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div className='px-4 py-3 z-10'>
+            <h1 className='text-md font-medium '>Loading...</h1>
+        </div>
+    )
+}
+
+export default Loader
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { Bebas_Neue } from 'next/font/google'
 import Image from 'next/image'
@@ -5,11 +7,31 @@ import { PiLinkLight } from "react-icons/pi";
 import { PiGithubLogoThin } from "react-icons/pi";
 import Link from 'next/link';
 
+
 const bebas = Bebas_Neue({ subsets: ['latin'], weight: '400' })
 const Projects = () => {
+
+    // const staticData: any = [
+    //     "apple", "banana", "cherry", "orange", "watermelon", "berry", "peach"
+    // ]
+
+    // const fetchSuggestions = async (query: any) => {
+    //     const response = await fetch(`https://dummyjson.com/recipes/search?q=${query}`);
+    //     if (response.ok) {
+    //         const result = await response.json();
+    //         return result.recipes;
+    //     } else {
+    //         throw new Error('Network Error');
+    //     }
+    // }
     return (
         <div id='projects' className='max-w-[1100px] mx-[4vw] lg:mx-auto pb-20'>
-            <h1 className={`${bebas.className} text-center w-[11rem] mx-auto text-[3.5rem] border-b-2 border-gray-500`}>Projects</h1>
+            <div className='flex flex-col gap-8 sm:flex-row items-center sm:justify-center sm:gap-[20vw] lg:justify-evenly'>
+                <h1 className={`${bebas.className} text-center w-[11rem] text-[3.5rem] border-b-2 border-gray-500`}>Projects</h1>
+                {/* <AutoComplete placeholder={"Enter Recipe"} staticData={staticData} fetchSuggestions={fetchSuggestions} dataKey={"name"} onSelect={(e: any) => console.log(e)} onChange={(input: any) => { }} customLoading={''} /> */}
+                {/* onSelect={(res) => console.log(res)} */}
+            </div>
+
             <div className='flex flex-wrap gap-4 items-center justify-center my-6 '>
                 <div className='duration-1000 delay-150 w-full sm:w-[18rem] md:w-auto flex flex-col gap-4 justify-center rounded-md shadow-md shadow-gray-200 px-4 py-6'>
                     <Image src='/project1.png' alt='project-img' width={500} height={500} className='w-full sm:w-[20rem] rounded-md hover:contrast-50 hover:transform hover:ease-in' />
@@ -44,6 +66,18 @@ const Projects = () => {
                         </div>
                     </div>
                     <p className='text-[0.85rem] text-gray-400 text-wrap'>NEXTjs TAILWIND TYPESCRIPT<br />MONGODB NODEMAILER </p>
+
+                </div>
+                <div className='duration-1000 delay-150 w-full sm:w-[18rem] md:w-auto flex flex-col gap-4 justify-center rounded-md shadow-md shadow-gray-200 px-4 py-4'>
+                    <Image src='/project6.png' alt='project-img' width={500} height={500} className='w-[60vw] mx-auto sm:w-[15rem] h-[50vw] sm:h-[9rem] md:h-[9rem] rounded-md hover:contrast-50' />
+                    <div className='flex items-center justify-between gap-4'>
+                        <p className='text-[1rem] font-bold text-black'>RAG <br /><span className='font-light'>Chatbot</span></p>
+                        <div className='flex gap-4 items-center pr-1'>
+                            <Link href='https://chatrag.vercel.app'><PiLinkLight size={26} /></Link>
+                            <Link href='https://github.com/SnehansuBehera/RAG-Chatbot'><PiGithubLogoThin size={26} /></Link>
+                        </div>
+                    </div>
+                    <p className='text-[0.85rem] text-gray-400 text-wrap'>NEXTjs TAILWIND <br />SUPABASE VERCEL AI SDK </p>
 
                 </div>
                 <div className='duration-1000 delay-150 w-full sm:w-[18rem] md:w-auto flex flex-col gap-4 justify-center rounded-md shadow-md shadow-gray-200 px-4 py-4'>
